e016564680641c7dc5b4aca4e17067789a4dbf58
Ijaz <180054r@uom.lk>
2023-05-12 20:58:06 +0530

    working code

--- script.py
+++ script.py
@@ -2,7 +2,7 @@ from asyncio import subprocess
 import math
 import random
 import matplotlib.pyplot as plt
-import gapopulation
+from population import GAPopulation 
 from Input import getInput
 
 from CostCalculator import CostCalculator
@@ -11,10 +11,10 @@ from CostCalculator import CostCalculator
 T = 24
 M1=0
 M2=0
-EPS = pow(10,-30)
+
 CHARGING_LEVELS = 10
 
-min_f = pow(10,10)
+
 # Data for the microgrid
 solar_generation = []
 soc_0 = 0
@@ -45,40 +45,32 @@ if __name__ == '__main__':
 
         min_f = pow(10,10)
 
-        N,soc_0,solar_generation,load_consumption,actual_solar,actual_building,electricity_tariff,shiftable_loads,sheddable_loads = getInput(d)
+        T,soc_0,solar_generation,load_consumption,actual_solar,actual_building,electricity_tariff,shiftable_loads,sheddable_loads = getInput(d)
         M1 = len(shiftable_loads)
         M2 = len(sheddable_loads)
         random.seed(666)
-        calculator = CostCalculator(N,soc_0,solar_generation,load_consumption,actual_solar,actual_building,electricity_tariff,shiftable_loads,sheddable_loads)
+        calculator = CostCalculator(T,soc_0,solar_generation,load_consumption,actual_solar,actual_building,electricity_tariff,shiftable_loads,sheddable_loads)
         battery_idle_cost.append(calculator.calculate_total_cost({'battery_schedule':[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],'shed_l_schedule':[],'shift_l_schedule':[]}))
-        """xx = []
-        yy = []
-        fname = 'graph.png'
+      
 
-        if len(sys.argv) > 1:
-            plot(xx,yy,fname)
+        
 
-        population = gapopulation.Population()
-        population.init_population(N,p_size)
+        population = GAPopulation(T, M1, M2,calculator)
+        population.init_population(p_size)
   
         for iteration in range(n_iterations): # iteration = echo
             print()
             print("######################Generation",iteration,"######################")
             print()
-            xx.append(iteration)
-            yy.append(population.get_avg())
-            plot(xx,yy,fname)
+            
 
             population = population.next_generation(p_size)
-            if(iteration %10==0):  
+            if(iteration %1==0):  
                 population.print_stats()
 
         #optimal_cost.append() max(0,calculate_total_cost(population.get_best()[0])) # for Sri Lanka
         optimal_cost.append(calculate_total_cost(population.get_best()[0])) # for Australia
-        # if population.get_best()[1] == 0:
-            #    break
-        """
-#print("idle vs optimal ",battery_idle_cost,optimal_cost)
-#print("idle vs optimal ",sum(battery_idle_cost),sum(optimal_cost))
+
+    
 
 print("idle vs optimal ",battery_idle_cost)
