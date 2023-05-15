1c6dfb822c71218ae5730482907396229a3aa0e3
mudithnirmala <mudithnirmala@gmail.com>
2023-05-15 17:48:59 +0530

    Completed get_grid_load function in LoadManager Class

--- loads.py
+++ loads.py
@@ -5,35 +5,40 @@ class LoadManager:
         self.shiftable_loads = shiftable_loads
         self.load_consumption = load_consumption
         self.solar_generation = solar_generation
-        self.schedule = [0 for _ in range(T)]
-        self.battery_charging_rates = [0 for _ in range(T)]
 
-    def add_load(self, start_time, period, consumption):
+    def add_load(self,schedule, start_time, period, consumption):
         for i in range(start_time, start_time + period):
-            self.schedule[i % self.T] += consumption
+            schedule[i % self.T] += consumption
 
-    def remove_load(self, start_time, period, consumption):
+    def remove_load(self, schedule,start_time, period, consumption):
         for i in range(start_time, start_time + period):
-            self.schedule[i % self.T] -= consumption
+            schedule[i % self.T] -= consumption
 
-    def add_generation(self, start_time, period, generation):
+    def add_generation(self, schedule, start_time, period, generation):
         for i in range(start_time, start_time + period):
-            self.schedule[i % self.T] += generation
+            schedule[i % self.T] += generation
 
-    def add_battery_power(self, battery_charging_rates):
+    def add_battery_power(self, schedule, battery_charging_rates):
         for i in range(self.T):
-            self.battery_charging_rates[i] += battery_charging_rates[i]
-    
-
-    def get_shifted_load():
-        return
-
-    
+            schedule[i] += battery_charging_rates[i]
 
     def get_grid_load(self,creature):
         return [max(0, self.load_consumption[i] - self.schedule[i] - self.solar_generation[i] + self.battery_charging_rates[i]) for i in range(self.T)]
 
     def get_grid_load(self, creature):
-        shed_load = [self.sheddable_loads[i] if creature['shed_l_schedule'][i] == 1 else 0 for i in range(self.T)]
-        grid_load = [max(0, self.load_consumption[i] - self.schedule[i] - self.solar_generation[i] + self.battery_charging_rates[i] - shed_load[i]) for i in range(self.T)]
+        schedule = [0 for i in range(self.T)] 
+        shed_loads = [self.sheddable_loads[i] if creature['shed_l_schedule'][i] == 1 else 0 for i in range(self.T)]
+        shift_loads = creature['shift_l_schedule']
+
+        battery_charging_rates = creature["battery_schedule"]
+        self.add_battery_power(schedule, battery_charging_rates)
+
+        for l in shed_loads:
+            self.remove_load(schedule, l["start_time"],l["period"],l["consumption"])
+
+        for l in shift_loads:
+            self.add_load(schedule, l["start_time"],l["period"],l["consumption"])
+
+        grid_load = [max(0, self.load_consumption[i] + schedule[i] - self.solar_generation[i]) for i in range(self.T)]
+        
         return grid_load
