30f81c4fa6040c624cd569cc0418e76070471a38
Ijaz <180054r@uom.lk>
2023-05-23 16:52:08 +0530

    edit

--- CostCalculator.py
+++ CostCalculator.py
@@ -1,42 +1,9 @@
 class CostCalculator:
-<<<<<<< HEAD
-    BATTERY_CAPACITY = 400000
-    def __init__(self,T, electricity_tariff,penalties,diesel_capacity=100000,diesel_unit_cost=120):
-=======
     def __init__(self,T, electricity_tariff,sheddable_penalties,load_manager,diesel_unit_cost=0.120):
->>>>>>> main_sl
         
         self.electricity_tariff = electricity_tariff
         self.T = T
         self.diesel_unit_cost = diesel_unit_cost
-<<<<<<< HEAD
-        self.diesel_capacity = diesel_capacity
-        self.penalties = penalties
-
-    def calculate_bill(self,grid_load):
-        rates = self.electricity_tariff
-        #grid_sell_back_rate = 37
-        bill=0
-        for i in range(len(grid_load)):
-            bill+=rates[i]*grid_load[i]/1000
-
-        return bill
-
-    def calculate_diesel_cost(self,diesel_gen_period):
-        return self.diesel_unit_cost*self.diesel_capacity*diesel_gen_period
-
-    def calculate_load_shedding_penalties(self,shed_loads):
-        p = 0
-        for l in shed_loads:
-            p+= self.penalties[l]
-
-        return p
-        
-    def get_total_cost(self,grid_load,shed_loads,diesel_gen_period):
-        
-        return self.calculate_bill(grid_load) + self.calculate_diesel_cost(diesel_gen_period)+self.calculate_load_shedding_penalties(shed_loads)
-       
-=======
         self.sheddable_penalties = sheddable_penalties
         self.load_manager = load_manager
 
@@ -56,4 +23,3 @@ class CostCalculator:
         shedded_loads = [i if chromosome['shed_l_schedule'][i] == 1 else 0 for i in range(len(chromosome['shed_l_schedule']))]
         
         return self.calculate_bill(grid_load) + self.calculate_diesel_cost(diesel_units) + self.calculate_shed_penalties(shedded_loads)
->>>>>>> main_sl
--- Input.py
+++ Input.py
@@ -1,65 +1,3 @@
-<<<<<<< HEAD
-import csv
-def getInput(d):
-
-
-    with open('input.txt','r') as file:
-        N = int(file.readline().strip())
-        battery_state_0 = int(file.readline().strip())
-        solar_generation = list(map(int,file.readline().strip().split()))
-        load_consumption = list(map(int,file.readline().strip().split()))
-        electricity_tariff = list(map(int,file.readline().strip().split()))
-
-        shiftable_loads=[]
-        sheddable_loads=[]
-
-        M1 = int(file.readline().strip())
-        for i in range(M1):
-            load_data = {}
-
-            start, end, duration, consumption = map(int, file.readline().strip().split())
-            load_data['start'] = start
-            load_data['end'] = end
-            load_data['duration'] = duration
-            load_data['consumption'] = consumption
-
-            shiftable_loads.append(load_data)
-
-        M2 = int(file.readline().strip())
-        for i in range(M2):
-            load_data = {}
-
-            start, end, consumption, penalty = map(int, file.readline().strip().split())
-            load_data['start'] = start
-            load_data['end'] = end
-            load_data['consumption'] = consumption
-            load_data['penalty'] = penalty
-
-            sheddable_loads.append(load_data)
-
-    solar_forecasting = []
-    actual_solar = []
-    building_forecasting = []
-    actual_building = []
-
-    with open('february_data.csv', newline='') as csvfile:
-        reader = csv.DictReader(csvfile)
-        for row in reader:
-            solar_forecasting.append(float(row['Solar Forecasting']))
-            actual_solar.append(float(row['Actual Solar']))
-            building_forecasting.append(float(row['Building Forecasting']))
-            actual_building.append(float(row['Actual Building']))
-
-    solar_forecasting = solar_forecasting[24*d:24*(d+1)]
-    actual_solar = actual_solar[24*d:24*(d+1)]
-    building_forecasting = building_forecasting[24*d:24*(d+1)]
-    actual_building = actual_building[24*d:24*(d+1)]
-
-    #return N,battery_state_0,solar_generation,load_consumption,electricity_tariff,shiftable_loads,sheddable_loads
-    return N,battery_state_0,solar_forecasting,building_forecasting,actual_solar,actual_building, electricity_tariff,shiftable_loads,sheddable_loads
-
-
-=======
 import csv
 import pandas as pd
 battery_state_0 = 50
@@ -124,4 +62,3 @@ def getInput(d):
     return N,battery_state_0,solar_forecasting,building_forecasting,actual_solar,actual_building, electricity_tariff,shiftable_loads,sheddable_loads
 
 
->>>>>>> main_sl
