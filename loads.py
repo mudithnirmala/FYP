600d9998f51f4914a07eaee9f4d761958a62e18c
mudithnirmala <mudithnirmala@gmail.com>
2023-05-23 16:06:51 +0530

    edit

--- loads.py
+++ loads.py
@@ -1,19 +1,19 @@
 class LoadManager:
-    def __init__(self, T, sheddable_loads, shiftable_loads, load_consumption, solar_generation,grid_disconnection_period):
+    def __init__(self, T, sheddable_loads, shiftable_loads, load_consumption, solar_generation,grid_disconnection_duration):
         self.T = T
         self.sheddable_loads = sheddable_loads
         self.shiftable_loads = shiftable_loads
         self.load_consumption = load_consumption
         self.solar_generation = solar_generation
         self.BATTERY_CAPACITY = 400000
-        self.grid_disconnection_period = grid_disconnection_period
+        self.grid_disconnection_duration = grid_disconnection_duration
 
-    def add_load(self,schedule, start_time, period, consumption):
-        for i in range(start_time, start_time + period):
+    def add_load(self,schedule, start_time, duration, consumption):
+        for i in range(start_time, start_time + duration):
             schedule[i % self.T] += consumption
 
-    def remove_load(self, schedule,start_time, period, consumption):
-        for i in range(start_time, start_time + period):
+    def remove_load(self, schedule,start_time, duration, consumption):
+        for i in range(start_time, start_time + duration):
             schedule[i % self.T] -= consumption
 
     def add_battery_power(self, schedule, battery_charging_rates):
@@ -23,7 +23,7 @@ class LoadManager:
     def get_diesel_units(self, creature):
         diesel_units =0
         grid_load = self.get_grid_load(creature)
-        for i in range(self.grid_disconnection_period[0], self.grid_disconnection_period[1]):
+        for i in range(self.grid_disconnection_duration[0], self.grid_disconnection_duration[1]):
             diesel_needed = grid_load[i]
             diesel_units+= diesel_needed
            
@@ -39,10 +39,10 @@ class LoadManager:
         self.add_battery_power(net_load_adjustments, battery_charging_rates)
 
         for l in shed_loads:
-            self.remove_load(net_load_adjustments, l["start_time"],l["period"],l["consumption"])
+            self.remove_load(net_load_adjustments, l["start_time"],l["duration"],l["consumption"])
 
         for l in shift_loads:
-            self.add_load(net_load_adjustments, l["start_time"],l["period"],l["consumption"])
+            self.add_load(net_load_adjustments, l["start_time"],l["duration"],l["consumption"])
 
         grid_load = [max(0, self.load_consumption[i] + net_load_adjustments[i] - self.solar_generation[i]) for i in range(self.T)]
          
